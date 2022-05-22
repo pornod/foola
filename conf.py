ed82b66ecaac8ab9eda75a27d94b2993ba815fa0
Tnoob-dev <93684341+Tnoob-dev@users.noreply.github.com>
2022-05-21 23:27:47 -0400

    Add files via upload

---
+++ conf.py
@@ -0,0 +1,6 @@
+import os
+
+api_hash = os.environ.get("API_HASH", "")
+api_id = os.environ.get("API_ID", "")
+bot_token = os.environ.get("BOT_TOKEN", "")
+bot_admin_group = os.environ.get("BOT_ADMIN_GROUP", "")
