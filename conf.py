5df6a7195c82856787d6bce27f1dffd12104f592
Tnoob-dev <93684341+Tnoob-dev@users.noreply.github.com>
2022-06-09 16:57:37 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -3,4 +3,4 @@ import os
 api_hash = os.environ.get("API_HASH", "")
 api_id = os.environ.get("API_ID", "")
 bot_token = os.environ.get("BOT_TOKEN", "")
-bot_admin_group = -1001572562658
+bot_admin_group = -742865751
